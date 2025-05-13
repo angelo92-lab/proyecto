9312c69553d7d8f0e6dd563876236328833b6527
angeloko089 <angelokocortes@gmail.com>
2025-05-13 09:58:14 -0400

    tiempo6

--- app/Http/Controllers/ClimaController.php
+++ app/Http/Controllers/ClimaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class WeatherController extends Controller
+class ClimaController extends Controller
 {
     public function showTemperature()
     {
