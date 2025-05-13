663f6524288c75f9f7340584d43470c6569d3fad
angeloko089 <angelokocortes@gmail.com>
2025-05-13 16:23:54 -0400

    funcionarios7

--- app/Imports/FuncionariosImport.php
+++ app/Imports/FuncionariosImport.php
@@ -5,16 +5,22 @@ namespace App\Imports;
 use App\Models\Funcionario;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithCustomStartRow;
 
-class FuncionariosImport implements ToModel, WithHeadingRow
+class FuncionariosImport implements ToModel, WithHeadingRow, WithCustomStartRow
 {
     public function model(array $row)
     {
+        dd($row);
         return new Funcionario([
-            'rut' => $row['rut'],      // minúsculas si en el Excel están como "rut"
+            'rut' => $row['rut'],       // encabezado en minúscula
             'nombre' => $row['nombre']
         ]);
     }
+
+    // Le decimos que los encabezados están en la fila 2
+    public function startRow(): int
+    {
+        return 2;
+    }
 }
-
-
