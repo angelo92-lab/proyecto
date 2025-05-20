d64fe27f014b55dd6b32f21ada54f875ec42ad7b
angeloko089 <angelokocortes@gmail.com>
2025-05-20 12:06:13 -0400

    pepepepepepepepe

--- app/Models/PlanAcompanamiento.php
+++ app/Models/PlanAcompanamiento.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class PlanAcompanamiento extends Model
 {
     use HasFactory;
+
+    // Lista de atributos que se pueden asignar masivamente
+    protected $fillable = [
+        'curso',
+        'nombre',
+        'procedencia',
+        'asignatura',
+        'asistencia',
+        'acompanamiento',
+    ];
 }
