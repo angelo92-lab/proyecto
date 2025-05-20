b76e2929b2b377ff6555811506e874f79db2889b
angeloko089 <angelokocortes@gmail.com>
2025-05-20 14:19:16 -0400

    pipipipi

--- app/Imports/PlanAcompanamientoImport.php
+++ app/Imports/PlanAcompanamientoImport.php
@@ -3,21 +3,31 @@
 namespace App\Imports;
 
 use App\Models\PlanAcompanamiento;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Row;
+use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class PlanAcompanamientoImport implements ToModel, WithHeadingRow
+class PlanAcompanamientoImport implements OnEachRow, WithHeadingRow, WithStartRow
 {
-    public function model(array $row)
+    public function startRow(): int
     {
-        return new PlanAcompanamiento([
-            'curso'         => $row['curso'],
-            'nombre'        => $row['nombre'],
-            'procedencia'   => $row['procedencia'],
-            'asignatura'    => $row['asignatura'],
-            'asistencia'    => $row['asistencia'],
-            'acompanamiento'=> $row['acompanamiento'],
+        return 3; // <-- Encabezados en fila 3
+    }
+
+    public function onRow(Row $row)
+    {
+        $r = $row->toArray();
+
+        if (empty($r['curso']) || empty($r['nombre'])) return;
+
+        PlanAcompanamiento::create([
+            'curso'         => $r['curso'] ?? '',
+            'nombre'        => $r['nombre'] ?? '',
+            'procedencia'   => $r['procedencia'] ?? '',
+            'asignatura'    => $r['asignatura'] ?? '',
+            'asistencia'    => is_numeric($r['asistencia']) ? intval($r['asistencia']) : null,
+            'acompanamiento'=> $r['acompanamiento'] ?? '',
         ]);
     }
 }
-
