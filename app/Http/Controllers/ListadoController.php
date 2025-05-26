87375315f7dabe3e7762b625ea82282e077d6397
angeloko089 <angelokocortes@gmail.com>
2025-05-26 10:10:04 -0400

    dadada

--- app/Http/Controllers/ListadoController.php
+++ app/Http/Controllers/ListadoController.php
@@ -9,9 +9,17 @@ class ListadoController extends Controller
     public function index()
     {
         $rutaArchivo = public_path('documentos/listadoestudiantes.xlsx');
+        if (!file_exists($rutaArchivo)) {
+    dd("El archivo no existe en: $rutaArchivo");
+} else {
+    dd("Archivo existe y se lee desde: $rutaArchivo");
+}
+        
         $documento = \PhpOffice\PhpSpreadsheet\IOFactory::load($rutaArchivo);
-        $hoja = $documento->getActiveSheet();
-        $todasLasFilas = $hoja->toArray();
+$hoja = $documento->getActiveSheet();
+$todasLasFilas = $hoja->toArray();
+
+dd($todasLasFilas);
 
         // Encabezados están en fila 5 (índice 4)
         $encabezados = $todasLasFilas[4];
@@ -19,9 +27,12 @@ class ListadoController extends Controller
         // Datos desde fila 6 en adelante (índice 5+)
         $datos = array_slice($todasLasFilas, 5);
 
-        // Limitamos a las primeras 20 filas para que no sea mucho
-        $datosMostrar = array_slice($datos, 0, 20);
+            // Limitamos a las primeras 20 filas para que no sea mucho
+            $datosMostrar = array_slice($datos, 0, 20);
 
-        return view('funcionarios.listado', compact('encabezados', 'datosMostrar'));
+        return view('funcionarios.listado', [
+    'encabezados' => $encabezados,
+    'datosMostrar' => $datosMostrar
+]);
     }
 }
