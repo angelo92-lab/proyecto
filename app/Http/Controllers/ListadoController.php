258c9b434fbedd6469a1dfdcb6539f2e31583bbe
angeloko089 <angelokocortes@gmail.com>
2025-05-26 11:19:57 -0400

    ???

--- app/Http/Controllers/ListadoController.php
+++ app/Http/Controllers/ListadoController.php
@@ -9,7 +9,7 @@ class ListadoController extends Controller
 {
     public function index()
     {
-        $rutaArchivo = public_path('documentos/listadoestudiantes.xlsx');
+        $rutaArchivo = public_path('documentos/listadodesfile.xlsx');
 
         if (!file_exists($rutaArchivo)) {
             abort(404, "Archivo no encontrado en: $rutaArchivo");
