21c04319074180ae34ad5989c7d016d43e34b8af
angeloko089 <angelokocortes@gmail.com>
2025-06-02 14:48:29 -0400

    bro dale

--- routes/web.php
+++ routes/web.php
@@ -193,7 +193,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/matricula/reportes', [MatriculaController::class, 'reportes'])->name('matricula.reportes');
     Route::get('/matricula/exportar-pdf', [MatriculaController::class, 'exportarPDF'])->name('matricula.exportar.pdf');
     Route::get('/matriculas/{id}', [MatriculaController::class, 'show'])->name('matricula.show');
-   Route::get('/matricula2026/{id}/pdf', [Matricula2026Controller::class, 'descargarPDF'])->name('matricula.pdf');
+   Route::get('/matricula2026/{id}/pdf', [MatriculaController::class, 'descargarPDF'])->name('matricula.pdf');
 
 });
 
