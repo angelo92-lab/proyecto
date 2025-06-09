36fa7730bce8761fd76c78e42d61fc679312911f
angeloko089 <angelokocortes@gmail.com>
2025-06-09 08:55:33 -0400

    ???

---
+++ resources/views/funcionarios/utp/planificaciones/basica.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container py-4">
+    <h2 class="mb-4">📘 Primera Unidad - Educación Básica</h2>
+
+    @if($carpetas->isEmpty())
+        <p class="text-muted">No hay carpetas disponibles por el momento.</p>
+    @else
+        <div class="list-group">
+            @foreach($carpetas as $carpeta)
+                <a href="{{ url('planificaciones/basica/' . $carpeta) }}" class="list-group-item list-group-item-action">
+                    📁 {{ $carpeta }}
+                </a>
+            @endforeach
+        </div>
+    @endif
+</div>
+@endsection
