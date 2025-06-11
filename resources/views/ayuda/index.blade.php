2fde65e6a0bf6446f69bff14ac8ff86fabafa5dd
angeloko089 <angelokocortes@gmail.com>
2025-06-11 11:45:58 -0400

    ayuda

--- resources/views/ayuda/index.blade.php
+++ resources/views/ayuda/index.blade.php
@@ -5,19 +5,7 @@
     <h2 class="titulo-principal">📖 Centro de Ayuda</h2>
 
     <div class="list-group mb-4">
-        <a class="list-group-item list-group-item-action" data-bs-toggle="collapse" href="#ayuda1" role="button">
-            📁 ¿Cómo subir una planificación?
-        </a>
-        <div class="collapse" id="ayuda1">
-            <div class="card card-body">
-                1. Ir al Portal de Funcionarios → Unidad Técnico Pedagógica → Planificaciones.<br>
-                2. Navegar hasta la carpeta correspondiente (Ej: 2° Básico).<br>
-                3. Click en “Subir archivo” y seleccionar el archivo Word o PDF.<br>
-                <strong>Nota:</strong> El archivo debe tener nombre claro, como "Lenguaje Unidad 1".
-            </div>
-        </div>
-
-        <a class="list-group-item list-group-item-action" data-bs-toggle="collapse" href="#ayuda2" role="button">
+       <a class="list-group-item list-group-item-action" data-bs-toggle="collapse" href="#ayuda2" role="button">
             📁 ¿Cómo marcar almuerzo?
         </a>
         <div class="collapse" id="ayuda2">
@@ -28,11 +16,24 @@
             </div>
         </div>
 
+        <a class="list-group-item list-group-item-action" data-bs-toggle="collapse" href="#ayuda2" role="button">
+            📁 ¿Cómo agregar y ver anotaciones? 
+        </a>
+        <div class="collapse" id="ayuda2">
+            <div class="card card-body">
+                1. Ir al menú superior y hacer clic en “Anotaciones o en el boton anotacion de la lista de alumnos”.<br>
+                2. Buscar al alumno por nombre<br>
+                3. EScribir la anotacion.
+                4. Apretar "Guardar Anotacion"
+                5. En anotaciones ver la lista de anotaciones de cada alumno.
+            </div>
+        </div>
+
         <!-- Puedes agregar más bloques similares -->
     </div>
 
     <div class="alert alert-info">
-        ¿Tienes dudas que no aparecen aquí? Contacta a soporte@tucorreo.cl
+        ¿Tienes dudas que no aparecen aquí? Contacta a angelokocortes@gmail.com
     </div>
 </div>
 @endsection
